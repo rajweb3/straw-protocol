d919a68220a89a4034f75eb2d42e7ce504580a64
boghranirav-kohin <nirav@kohin.xyz>
2024-11-16 23:10:52 +0700

    Add Lit Protocol

--- backend/env.d.ts
+++ backend/env.d.ts
@@ -1,8 +1,10 @@
 declare global {
-    namespace NodeJS {
-      interface ProcessEnv {
-        PORT: string;
-        NODE_ENV: string;
-        DATABASE_URL: string;}
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string;
+      NODE_ENV: string;
+      DATABASE_URL: string;
+      STRAW_PRIVATE_KEY: string;
     }
+  }
 }
