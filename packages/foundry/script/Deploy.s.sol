184da4ab43ce83bde67d1a7c8ea9dfac03b1ee52
Aditya Vijaykumar <aditya@Adityas-MacBook-Pro-2.local>
2024-11-17 08:25:49 +0700

    feat: Final Straw Settlement Contracts

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -2,12 +2,20 @@
 pragma solidity ^0.8.19;
 
 import "./DeployHelpers.s.sol";
+import "../contracts/StrawSettler.sol";
 import { DeployYourContract } from "./DeployYourContract.s.sol";
 
 contract DeployScript is ScaffoldETHDeploy {
   function run() external {
-    DeployYourContract deployYourContract = new DeployYourContract();
-    deployYourContract.run();
+    StrawSettler strawSettler = new StrawSettler(
+            0x000000000022D473030F116dDEE9F6B43aC78BA3
+        );
+        console.logString(
+            string.concat(
+                "StrawSettler deployed at: ",
+                vm.toString(address(strawSettler))
+            )
+        );
 
     // deploy more contracts here
     // DeployMyContract deployMyContract = new DeployMyContract();
--- packages/foundry/script/DeployYourContract.s.sol
+++ packages/foundry/script/DeployYourContract.s.sol
@@ -1,17 +1,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../contracts/YourContract.sol";
+import "../contracts/StrawSettler.sol";
 import "./DeployHelpers.s.sol";
 
 contract DeployYourContract is ScaffoldETHDeploy {
-  // use `deployer` from `ScaffoldETHDeploy`
-  function run() external ScaffoldEthDeployerRunner {
-    YourContract yourContract = new YourContract(deployer);
-    console.logString(
-      string.concat(
-        "YourContract deployed at: ", vm.toString(address(yourContract))
-      )
-    );
-  }
+    // use `deployer` from `ScaffoldETHDeploy`
+    function run() external ScaffoldEthDeployerRunner {
+
+    }
 }
